f6f7e7bb2beddb5ea46d26da622f8d07a8f7c4e1
wilFarias <wilsonluiz.dsf@gmail.com>
2021-10-25 20:46:37 -0300

    Reference Method com método estático

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -29,12 +29,8 @@ public class Product {
 		this.price = price;
 	}
 	
-	public static boolean staticMethodPredicate(Product p) {
-		return p.getPrice() >= 100.0;
-	}
-	
-	public boolean nonStaticMethodPredicate() {
-		return price >= 100.0;
+	public static void staticPriceUpdate(Product p) {
+		p.setPrice(p.getPrice()*1.1);		
 	}
 
 	@Override
