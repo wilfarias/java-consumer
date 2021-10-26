717b2934a4cc95e2815ae9b5f51353ac23dad341
wilFarias <wilsonluiz.dsf@gmail.com>
2021-10-25 21:02:01 -0300

    Implementações de Consumer

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.function.Consumer;
 
 import entities.Product;
 import util.PriceUpdate;
@@ -16,10 +16,22 @@ public class Program {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
-		
-		list.forEach(Product:: nonStaticPriceUpdate);	//reference method com não método estático
-		
+
+		//list.forEach(new PriceUpdate()); // Implementação da interface
+
+		//list.forEach(Product::staticPriceUpdate); // reference method com método estático
+
+		//list.forEach(Product::nonStaticPriceUpdate); // reference method com não método estático
+
+		/*double factor = 1.1;
+		Consumer<Product> cons = p -> {
+			p.setPrice(p.getPrice() * factor);
+		};
+		list.forEach(cons);*/ 					// Expressão lambda declarada
+
+		list.forEach(p -> p.setPrice(p.getPrice() * 1.1)); // expressão lambda inline
+
 		list.forEach(System.out::println); // reference method
-		
+
 	}
 }
